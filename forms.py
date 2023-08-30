10131ccfe2108942505ac39c4603c504b19eeb07
kirillefremov <efremovk7569@gmail.com>
2023-08-30 21:21:10 +0200

    Remove email validator

--- forms.py
+++ forms.py
@@ -21,7 +21,7 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = StringField("Password", validators=[DataRequired()])
     submit = SubmitField("Let me in!")
 
